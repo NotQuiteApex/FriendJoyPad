69343d2f6f8edeb8125f8b53c997c719e29da152
Logan H-D <notquiteapex@gmail.com>
2020-12-17 19:02:41 -0500

    Added pins

---
+++ MCU/include/config.h
@@ -0,0 +1,32 @@
+// Pins for each of the buttons, sticks, etc.
+PIN button_a = 22; // A
+PIN button_b = 23; // B
+PIN button_x = 3;  // X
+PIN button_y = 21; // Y
+
+PIN button_up    = 22; // DPAD up
+PIN button_down  = 23; // DPAD down
+PIN button_left  = 3;  // DPAD left
+PIN button_right = 21; // DPAD right
+
+PIN stick_left_x = 39; // left stick left and right
+PIN stick_left_y = 36; // left stick up and down
+PIN stick_left_b = 32; // left stick button
+
+PIN stick_right_x = 0; // right stick left and right
+PIN stick_right_y = 0; // right stick up and down
+PIN stick_right_b = 0; // right stick button
+
+PIN shoulder_left_1 = 19; // left shoulder encoder pin 1
+PIN shoulder_left_2 = 18; // left shoulder encoder pin 2
+PIN shoulder_left_b = 40; // left shoulder button
+
+PIN shoulder_right_1 = 0; // right shoulder encoder pin 1
+PIN shoulder_right_2 = 0; // right shoulder encoder pin 2
+PIN shoulder_right_b = 0; // right shoulder button
+
+PIN trigger_left_a = 0; // left trigger analog input
+PIN trigger_left_b = 0; // left trigger digital input
+
+PIN trigger_right_a = 0; // right trigger analog input
+PIN trigger_right_b = 0; // right trigger digital input
--- MCU/src/main.cpp
+++ MCU/src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 
+#define PIN const byte
+
+#include "config.h"
+
 void setup() {
   // put your setup code here, to run once:
 }
