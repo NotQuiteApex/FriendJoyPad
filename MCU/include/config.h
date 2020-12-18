8ec71b9ee212a187a7b1715b63c790ff39f9f96b
Logan H-D <notquiteapex@gmail.com>
2020-12-17 23:34:52 -0500

    Add pairs for pinmodes

--- MCU/include/config.h
+++ MCU/include/config.h
@@ -1,32 +1,32 @@
 // Pins for each of the buttons, sticks, etc.
-PIN button_a = 22; // A
-PIN button_b = 23; // B
-PIN button_x = 3;  // X
-PIN button_y = 21; // Y
+PIN button_a = {22, INPUT_PULLUP}; // A
+PIN button_b = {23, INPUT_PULLUP}; // B
+PIN button_x = {3, INPUT_PULLUP};  // X
+PIN button_y = {21, INPUT_PULLUP}; // Y
 
-PIN button_up    = 22; // DPAD up
-PIN button_down  = 23; // DPAD down
-PIN button_left  = 3;  // DPAD left
-PIN button_right = 21; // DPAD right
+PIN button_up    = {22, INPUT_PULLUP}; // DPAD up
+PIN button_down  = {23, INPUT_PULLUP}; // DPAD down
+PIN button_left  = {3, INPUT_PULLUP};  // DPAD left
+PIN button_right = {21, INPUT_PULLUP}; // DPAD right
 
-PIN stick_left_x = 39; // left stick left and right
-PIN stick_left_y = 36; // left stick up and down
-PIN stick_left_b = 32; // left stick button
+PIN stick_left_x = {39, INPUT_PULLUP}; // left stick left and right
+PIN stick_left_y = {36, INPUT_PULLUP}; // left stick up and down
+PIN stick_left_b = {32, INPUT_PULLUP}; // left stick button
 
-PIN stick_right_x = 0; // right stick left and right
-PIN stick_right_y = 0; // right stick up and down
-PIN stick_right_b = 0; // right stick button
+PIN stick_right_x = {0, INPUT_PULLUP}; // right stick left and right
+PIN stick_right_y = {0, INPUT_PULLUP}; // right stick up and down
+PIN stick_right_b = {0, INPUT_PULLUP}; // right stick button
 
-PIN shoulder_left_1 = 19; // left shoulder encoder pin 1
-PIN shoulder_left_2 = 18; // left shoulder encoder pin 2
-PIN shoulder_left_b = 40; // left shoulder button
+PIN shoulder_left_1 = {19, INPUT_PULLUP}; // left shoulder encoder pin 1
+PIN shoulder_left_2 = {18, INPUT_PULLUP}; // left shoulder encoder pin 2
+PIN shoulder_left_b = {40, INPUT_PULLUP}; // left shoulder button
 
-PIN shoulder_right_1 = 0; // right shoulder encoder pin 1
-PIN shoulder_right_2 = 0; // right shoulder encoder pin 2
-PIN shoulder_right_b = 0; // right shoulder button
+PIN shoulder_right_1 = {0, INPUT_PULLUP}; // right shoulder encoder pin 1
+PIN shoulder_right_2 = {0, INPUT_PULLUP}; // right shoulder encoder pin 2
+PIN shoulder_right_b = {0, INPUT_PULLUP}; // right shoulder button
 
-PIN trigger_left_a = 0; // left trigger analog input
-PIN trigger_left_b = 0; // left trigger digital input
+PIN trigger_left_a = {0, INPUT_PULLUP}; // left trigger analog input
+PIN trigger_left_b = {0, INPUT_PULLUP}; // left trigger digital input
 
-PIN trigger_right_a = 0; // right trigger analog input
-PIN trigger_right_b = 0; // right trigger digital input
+PIN trigger_right_a = {0, INPUT_PULLUP}; // right trigger analog input
+PIN trigger_right_b = {0, INPUT_PULLUP}; // right trigger digital input
--- MCU/src/main.cpp
+++ MCU/src/main.cpp
@@ -1,13 +1,31 @@
 #include <Arduino.h>
 
-#define PIN const byte
+#include "Pair.h"
+
+#define PIN const Pair<byte, byte>
 
 #include "config.h"
 
-void setup() {
-  // put your setup code here, to run once:
+PIN pins[] = {
+  button_a, button_b, button_x, button_y,
+  button_up, button_down, button_left, button_right,
+  stick_left_x, stick_left_y, stick_left_b,
+  stick_right_x, stick_right_y, stick_right_b,
+  shoulder_left_1, shoulder_left_2, shoulder_left_b,
+  shoulder_right_1, shoulder_right_2, shoulder_right_b,
+  trigger_left_a, trigger_left_b,
+  trigger_right_a, trigger_right_b
+};
+
+void setup () {
+  // Init pins
+  for (byte i = 0; i < (sizeof(pins) / sizeof(pins[0])); i++) {
+    pinMode(pins[i].first(), pins[i].second());
+  }
+
+  // Init serial
+  Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop () {
 }
